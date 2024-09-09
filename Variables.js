a057c05666b7bd391cce31c123f7b584bb98898a
Allyza-lam <allyzanantuna56@gmail.com>
2024-09-09 23:04:09 +0800

    Create Variables.js

---
+++ Variables.js
@@ -0,0 +1,13 @@
+// Create a variable named name and store your name
+let name = 'Nantuna,Allyza J,';
+// Create a variable named whatDoYouWannaBecomeInYourLife and store "programmer"
+let whatDoYouWannaBecomeInYourLife = 'programmer,';
+// Create a variable named gender and store your gender
+let gender = 'Female,';
+// Create a variable named TwitterHandle and store your Twitter Handle
+let twitterhandle = '@yza22,';
+log all your console
+console.log('what is your name?',Allyza);
+console.log('what do you wanna becine in your life?',programmer);
+console.log('Are you male or female?',female);
+console.log('my twitter:',twitterhandle);
